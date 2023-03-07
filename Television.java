f0c27cbb3498ccd79194bfb708b1d7737fea9b33
Dongjun Kim <ansqufdl1992@naver.com>
2023-03-07 13:08:32 +0900

    first commit

---
+++ Television.java
@@ -0,0 +1,7 @@
+public class Television {
+    int channel;
+    int volume;
+    void print(){
+        System.out.println("채널은" + channel +"이고 볼륨은 " + volume + "입니다.");
+    }
+}
---
+++ TelevisionTest.java
@@ -0,0 +1,13 @@
+public class TelevisionTest {
+    public static void main(String[] args) {
+        Television myTv = new Television();
+        myTv.channel = 7;
+        myTv.volume = 9;
+        myTv.print();
+
+        Television yourTv = new Television();
+        yourTv.channel = 9;
+        yourTv.volume = 12;
+        yourTv.print();
+    }
+}
