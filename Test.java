192397f52d7263a2f6a9fac0bf89b70ab5e86cdd
Dongjun Kim <ansqufdl1992@naver.com>
2023-04-04 11:17:10 +0900

    과제

--- Test.java
+++ Test.java
@@ -8,7 +8,7 @@ public class Test {
     public static void main(String[] args){
         A obj = new A(); // 객체 생성
 
-        obj.a = 10; // 전용 멤버는 다른 클래스에서는 접근이 불가능하다.
+        // obj.a = 10; // 전용 멤버는 다른 클래스에서는 접근이 불가능하다.
         obj.b = 20; // 디폴트 멤버는 접근 가능하다.
         obj.c = 30; // 공용 멤버도 접근 가능하다.
     }
